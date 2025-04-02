533d0e61db43c18bb41bb1d1ba41a39e2e10c258
David Neon <sdneon@users.noreply.github.com>
2025-04-02 13:38:53 +0800

    renumbered some sections to tally with article

--- 4.6-inception/1-run_with_args.bat
+++ 4.6-inception/1-run_with_args.bat
@@ -1,11 +1,11 @@
 @ECHO ----------------------------------------------------------
 @ECHO There only needs to be:
 @ECHO   makeSea.exe in this folder,
-@ECHO   and 'input' sub-folder containing:
+@ECHO   and 'ocean' sub-folder containing:
 @ECHO   makeSea.js, node.zip, node_modules.zip
 @ECHO makeSea.js is in a sub-folder
 @ECHO   so that the output copy of makeSea.exe
 @ECHO   won't conflict with the original copy.
 @ECHO ----------------------------------------------------------
-makeSea.exe -i input\node.zip -i input\node_modules.zip input\makeSea.js
+makeSea.exe -i ocean\node.zip -i ocean\node_modules.zip ocean\makeSea.js
 @PAUSE
